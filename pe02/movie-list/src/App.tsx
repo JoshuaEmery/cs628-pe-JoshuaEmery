5de384f4775fefbb7766d99c0a180bd69e92293b
Joshua Emery <jemery@Joshuas-MacBook-Pro.local>
2023-07-13 20:24:38 -0700

    Console.log for selection finally working

---
+++ pe02/movie-list/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MovieList from "./Components/MovieList";
+
+function App() {
+  return (
+    <div>
+      <MovieList></MovieList>
+    </div>
+  );
+}
+
+export default App;
---
+++ pe02/movie-list/src/Components/MovieSelectList.tsx
@@ -0,0 +1,21 @@
+import { SyntheticEvent } from "react";
+
+interface MovieListItemProps {
+  Genres: string[];
+  onSelect: (genre: string) => void; // update here
+}
+function MovieSelectList(props: MovieListItemProps) {
+  // update here to handle selection
+  const handleSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    props.onSelect(event.target.value);
+  };
+  return (
+    <select onChange={handleSelection} name="" id="">
+      {props.Genres.map((genre) => {
+        return <option value={genre}>{genre}</option>;
+      })}
+    </select>
+  );
+}
+
+export default MovieSelectList;
